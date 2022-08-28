830f54ac48a5d1ba3f2610c4ee9d5bf62a012545
magnopachecoluiz <magno.luiz@nicephotos.net>
2022-08-28 13:44:21 -0300

    wip solving

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, scss,ts}"],
+  content: [
+    "./src/**/*.{html, scss,ts}",
+  ],
   theme: {
     fontFamily: {
       Nunito: ['Nunito', 'sans-serif'],
